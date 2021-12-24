bff4ae0e3eeaaffa4c36143680c9b71ff889f9f8
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-12-24 10:56:12 +0700

    stash pop

--- app/routes/route.go
+++ app/routes/route.go
@@ -2,16 +2,22 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"profcourse/controllers/users"
 )
 
 type ControllerList struct {
+	JWTMiddleware  middleware.JWTConfig
 	UserController users.UserController
 }
 
 func (cl *ControllerList) RouteRegister(e *echo.Echo) {
+
 	ev1 := e.Group("api/v1/")
-	ev1.POST("users", cl.UserController.CreateUser)
 	ev1.POST("login", cl.UserController.Login)
 	ev1.PUT("forgetpassword", cl.UserController.ForgetPassword)
+
+	withJWT := ev1.Group("")
+	withJWT.Use(middleware.JWTWithConfig(cl.JWTMiddleware))
+	withJWT.POST("users", cl.UserController.CreateUser)
 }
