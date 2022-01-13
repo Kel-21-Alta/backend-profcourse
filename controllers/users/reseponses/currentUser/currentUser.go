45677051f71985286a86b7638c5b978ee4f3acaa
wahyunf354 <wahyunurfadillah313@gmail.com>
2022-01-13 13:02:40 +0700

    selesai menambahkan response current user yaitu key nohp dan memperbaiki format birth

--- controllers/users/reseponses/currentUser/currentUser.go
+++ controllers/users/reseponses/currentUser/currentUser.go
@@ -6,14 +6,15 @@ import (
 )
 
 type UserCreated struct {
-	ID         string    `json:"user_id"`
-	Name       string    `json:"name"`
-	Email      string    `json:"email"`
-	RoleText   string    `json:"role"`
-	Bio        string    `json:"bio"`
-	UrlImage   string    `json:"url_image"`
-	Birth      time.Time `json:"birth"`
-	BirthPlace string    `json:"birth_place"`
+	ID         string `json:"user_id"`
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+	RoleText   string `json:"role"`
+	Bio        string `json:"bio"`
+	UrlImage   string `json:"url_image"`
+	Birth      string `json:"birth"`
+	NoHp       string `json:"no_hp"`
+	BirthPlace string `json:"birth_place"`
 }
 
 func FromDomain(domain users.Domain) UserCreated {
@@ -24,7 +25,8 @@ func FromDomain(domain users.Domain) UserCreated {
 		RoleText:   domain.RoleText,
 		Bio:        domain.Bio,
 		UrlImage:   domain.ImgProfile,
-		Birth:      domain.Birth,
+		Birth:      domain.Birth.Format(time.RFC822),
+		NoHp:       domain.NoHp,
 		BirthPlace: domain.BirthPlace,
 	}
 }
