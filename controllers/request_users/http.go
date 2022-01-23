fa9b1bfda7f0d9778d92ae7d447760d1cda7c0c2
wahyunf354 <wahyunurfadillah313@gmail.com>
2022-01-23 23:42:38 +0700

    membuat controller create request user

---
+++ controllers/request_users/http.go
@@ -0,0 +1,46 @@
+package requestusers
+
+import (
+	"net/http"
+	"profcourse/app/middlewares"
+	"profcourse/business/request_users"
+	controller "profcourse/controllers"
+	"profcourse/controllers/request_users/requests"
+
+	"github.com/labstack/echo/v4"
+)
+
+type RequestUserController struct {
+	RequestUserUsecase request_users.Usecase
+}
+
+func NewRequestUserController(usecase request_users.Usecase) RequestUserController {
+	return RequestUserController{RequestUserUsecase: usecase}
+}
+
+func (ctr *RequestUserController) CreateRequest(c echo.Context) error {
+
+	var req requests.CreateRequestUser
+
+	if err := c.Bind(&req); err != nil {
+		return controller.NewResponseError(c, err)
+	}
+
+	token, err := middlewares.ExtractClaims(c)
+
+	if err != nil {
+		return controller.NewResponseError(c, err)
+	}
+
+	var domain = req.ToDomain()
+	domain.UserId = token.Userid
+
+	ctx := c.Request().Context()
+	result, err := ctr.RequestUserUsecase.CreateRequest(ctx, &domain)
+
+	if err != nil {
+		return controller.NewResponseError(c, err)
+	}
+
+	return controller.NewResponseSuccess(c, http.StatusOK, result)
+}
---
+++ controllers/request_users/responses/createRequestuser/response.go
@@ -0,0 +1,13 @@
+package createrequestuser
+
+import "profcourse/business/request_users"
+
+type CreateRequestUser struct {
+	Topik             string `json:"topik"`
+	CategoryRequestId string `json:"category_request_id"`
+	CategoryRequest   string `json:"category_request"`
+}
+
+func FromDomain(domain request_users.Domain) *CreateRequestUser {
+	return &CreateRequestUser{Topik: domain.Request, CategoryRequestId: domain.Category.ID, CategoryRequest: domain.Category.Title}
+}
