ed8af014ca9f57fe13e260cc5f7e68dbeae389f9
wahyunf354 <wahyunurfadillah313@gmail.com>
2022-01-24 00:07:58 +0700

    selesai membut driver create request user

---
+++ drivers/databases/request_users/mysql.go
@@ -0,0 +1,31 @@
+package requestusers
+
+import (
+	"context"
+	"profcourse/business/request_users"
+
+	"gorm.io/gorm"
+)
+
+type RequestUserRepo struct {
+	Conn *gorm.DB
+}
+
+func (r *RequestUserRepo) CreateRequest(ctx context.Context, domain *request_users.Domain) (request_users.Domain, error) {
+
+	var rec = FromDomain(domain)
+
+	err := r.Conn.Create(&rec).Error
+
+	if err != nil {
+		return request_users.Domain{}, err
+	}
+
+	return rec.ToDomain(), nil
+}
+
+func NewRequestUserRepository(conn *gorm.DB) request_users.Repository {
+	return &RequestUserRepo{
+		Conn: conn,
+	}
+}
