84e12cbc3b2677c9780d794f31ed84df98f10f5c
wahyunf354 <wahyunurfadillah313@gmail.com>
2022-01-25 11:34:20 +0700

    selesai membuat unit test

--- business/request_users/mocks/Repository.go
+++ business/request_users/mocks/Repository.go
@@ -14,6 +14,29 @@ type Repository struct {
 	mock.Mock
 }
 
+// AdminGetAllRequestUser provides a mock function with given fields: ctx, domain
+func (_m *Repository) AdminGetAllRequestUser(ctx context.Context, domain *request_users.Domain) ([]request_users.Domain, error) {
+	ret := _m.Called(ctx, domain)
+
+	var r0 []request_users.Domain
+	if rf, ok := ret.Get(0).(func(context.Context, *request_users.Domain) []request_users.Domain); ok {
+		r0 = rf(ctx, domain)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]request_users.Domain)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *request_users.Domain) error); ok {
+		r1 = rf(ctx, domain)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // CreateRequest provides a mock function with given fields: ctx, domain
 func (_m *Repository) CreateRequest(ctx context.Context, domain *request_users.Domain) (request_users.Domain, error) {
 	ret := _m.Called(ctx, domain)
